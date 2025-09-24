4a8843492a8f02782e1b7ba5d490d6d326c46fb4
fujimotoAr <theofilus.arkhi@mail.ugm.ac.id>
2025-09-24 13:35:04 +0700

    switch segmentation to otsu

--- dataset.py
+++ dataset.py
@@ -1,39 +1,44 @@
 import os, cv2, numpy as np, pandas as pd, matplotlib.pyplot as plt
 
+kernel_sharpen = np.array([[-1, -1, -1],
+                    [-1, 8, -1],
+                    [-1, -1, -1]])*-1
+
+def multiplication(kernel, submatrix):
+    kernel_size = len(kernel)
+    sum = 0
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            sum += kernel[i][j]*submatrix[i][j]
+    return sum
+
+def convolution(kernel, matrix):
+    kernel_size = len(kernel)
+    padded = np.zeros((len(matrix)+2, len(matrix[0])+2))
+    ret = np.zeros_like(padded)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            padded[i+1][j+1] = matrix[i][j]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            minplus = kernel_size//2
+            start_y = i+1-minplus
+            start_x = j+1-minplus
+            ret[i+1][j+1] = multiplication(
+                kernel, 
+                [arr[start_x:start_x+kernel_size] for arr in padded[start_y:start_y+kernel_size]]
+                )
+    return np.round(ret)
+            
+
 def get_dataset():
     path_normal = "TB_Chest_Radiography_Database/Normal"
     path_tb = "TB_Chest_Radiography_Database/Tuberculosis"
     x_normal=[]
     x_tb=[]
     for i, img in enumerate(os.listdir(path_normal)):        
-        # Handle cropping
         image = cv2.imread(os.path.join(path_normal, img))
-        # gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-
-        # if len(temp)>0:
-        #     image = temp
-
-        # # Handle cropping to even smaller
-        # L2_image = cv2.imread(os.path.join(path_normal, img))
-        # gs = cv2.cvtColor(L2_image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200,L2gradient=True)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = L2_image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-        
-        # if len(temp)>0:
-        #     image = temp
-
         image = (image)/255
         c = 255 / (1+np.max(image))
         image = c*np.log(1+image)
@@ -46,39 +51,13 @@ def get_dataset():
 
         # image = cv2.equalizeHist(image)
         image = cv2.resize(image, (512, 512))
+        # image = convolution(kernel_sharpen, image)
+        # image = image+cv2.filter2D(image,-1,kernel_sharpen)
 
         x_normal.append(image)
     
     for i, img in enumerate(os.listdir(path_tb)):
-
-        # # Handle cropping
         image = cv2.imread(os.path.join(path_tb, img))
-        # gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-
-        # if len(temp)>0:
-        #     image = temp
-
-        # # Handle cropping to even smaller
-        # L2_image = cv2.imread(os.path.join(path_tb, img))
-        # gs = cv2.cvtColor(L2_image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200,L2gradient=True)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = L2_image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-        
-        # if len(temp)>0:
-        #     image = temp
-
         # Log transform
         image = (image)/255
         c = 255 / (1+np.max(image))
@@ -95,28 +74,12 @@ def get_dataset():
         image = cv2.resize(image, (512, 512))
         if image.shape != (512, 512):
             continue
+        # image = convolution(kernel_sharpen, image)
+        # image = image+cv2.filter2D(image,-1,kernel_sharpen)
+
         x_tb.append(image)
 
     return x_normal, x_tb
-    
-# def get_dataset_test():
-#     path_normal = "TBX11K/imgs/health"
-#     path_tb = "TBX11K/imgs/tb"
-#     x_normal=[]
-#     x_tb=[]
-#     for img in os.listdir(path_normal):
-#         image = cv2.imread(os.path.join(path_normal, img))
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         x_normal.append(image)
-    
-#     for img in os.listdir(path_tb):
-#         image = cv2.imread(os.path.join(path_tb, img))
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         if image.shape != (512, 512):
-#             continue
-#         x_tb.append(image)
-
-#     return x_normal, x_tb
 
 def get_dataset_test():
     path_normal = "TBX11K/imgs/health"
@@ -124,34 +87,7 @@ def get_dataset_test():
     x_normal=[]
     x_tb=[]
     for i, img in enumerate(os.listdir(path_normal)):        
-        # # Handle cropping
         image = cv2.imread(os.path.join(path_normal, img))
-        # gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-
-        # if len(temp)>0:
-        #     image = temp
-
-        # # Handle cropping to even smaller
-        # L2_image = cv2.imread(os.path.join(path_normal, img))
-        # gs = cv2.cvtColor(L2_image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200,L2gradient=True)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = L2_image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-        
-        # if len(temp)>0:
-        #     image = temp
-
         image = (image)/255
         c = 255 / (1+np.max(image))
         image = c*np.log(1+image)
@@ -164,38 +100,14 @@ def get_dataset_test():
 
         # image = cv2.equalizeHist(image)
         image = cv2.resize(image, (512, 512))
+        # image = convolution(kernel_sharpen, image)
+        # image = image+cv2.filter2D(image,-1,kernel_sharpen)
 
         x_normal.append(image)
     
     for i, img in enumerate(os.listdir(path_tb)):
 
-        # # Handle cropping
         image = cv2.imread(os.path.join(path_tb, img))
-        # gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-
-        # if len(temp)>0:
-        #     image = temp
-
-        # # Handle cropping to even smaller
-        # L2_image = cv2.imread(os.path.join(path_tb, img))
-        # gs = cv2.cvtColor(L2_image, cv2.COLOR_BGR2GRAY)
-        # edges = cv2.Canny(gs,100,200,L2gradient=True)
-        # x, y, w, h = cv2.boundingRect(edges)
-        # temp = None
-        # try:
-        #     temp = L2_image[y:y+h, x:x+w, :].copy()
-        # except:
-        #     pass
-        
-        # if len(temp)>0:
-        #     image = temp
 
         # Log transform
         image = (image)/255
@@ -213,6 +125,9 @@ def get_dataset_test():
         image = cv2.resize(image, (512, 512))
         if image.shape != (512, 512):
             continue
+        # image = convolution(kernel_sharpen, image)
+        # image = image+cv2.filter2D(image,-1,kernel_sharpen)
+
         x_tb.append(image)
 
     return x_normal, x_tb
--- segmentation.py
+++ segmentation.py
@@ -2,20 +2,5 @@ import cv2, numpy as np
 
 
 def get_segmentation(source):
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-
-    k = 2
-
-    image = np.array(source,dtype=np.float32)
-    pixel_values = np.array(image.reshape((-1, 3)) if len(image.shape) == 3 else image.reshape((-1, 1)))
-    pixel_values = np.float32(pixel_values)
-
-    retval, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_PP_CENTERS) 
-
-
-    centers = np.uint8(centers) 
-
-    segmented_data = centers[labels.flatten()] 
-
-    segmented_image = segmented_data.reshape((image.shape)) 
-    return segmented_image
+    ret3,th3 = cv2.threshold(source,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return th3
